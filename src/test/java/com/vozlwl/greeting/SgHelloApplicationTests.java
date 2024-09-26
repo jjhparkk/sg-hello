ce5a90278e17bb01e5e74d269f13a1fbf09bf1dc
hihi <hihi@gmail.com>
2024-09-26 14:57:03 +0900

    first commit

---
+++ src/test/java/com/vozlwl/greeting/SgHelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vozlwl.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgHelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
