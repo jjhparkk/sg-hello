ab2b784d8a9c00bf925c3cf38c15c97634317986
hihi <hihi@gmail.com>
2024-09-26 15:00:46 +0900

    aaaaaaaaaaaa

--- src/main/java/com/vozlwl/controller/HelloController.java
+++ src/main/java/com/vozlwl/controller/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HelloController {
    @GetMapping("/hello")
    public String helloPage(Model model) {
-      model.addAttribute("greeting", "Hiiiissii!");
+      model.addAttribute("greeting", "Hddiiiissii!");
       return "hello";
    }
 }
